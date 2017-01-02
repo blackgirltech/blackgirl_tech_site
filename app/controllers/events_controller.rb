857cc8ea57975b1909d1919e8151a1a567da757c
damzcodes <damzcodes@gmail.com>
2017-01-02 21:01:28 +0000

    order events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,8 @@
 class EventsController < ApplicationController
 
   def index
-    @workshops = Event.where(workshop: true)
-    @masterclasses = Event.where(masterclass: true)
+    @workshops = Event.where(workshop: true).order(date: :asc)
+    @masterclasses = Event.where(masterclass: true).order(date: :asc)
   end
   
 end
