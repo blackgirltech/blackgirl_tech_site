0658df62f73f9d16df8b319bb2d084c89e3db0f1
damzcodes <damzcodes@gmail.com>
2017-01-17 15:39:49 +0000

    add subscription model and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117112542) do
+ActiveRecord::Schema.define(version: 20170117153345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -687,6 +687,12 @@ ActiveRecord::Schema.define(version: 20170117112542) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["source", "medium", "campaign"], name: "index_sources_on_source_and_medium_and_campaign", unique: true, using: :btree
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "survey_takers", force: :cascade do |t|
