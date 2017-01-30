7c8fe4397c342bbe4993836192a5d32c1a615936
damzcodes <damzcodes@gmail.com>
2017-01-30 18:14:00 +0000

    database setup

--- db/migrate/20170117153345_create_subscriptions.rb
+++ db/migrate/20170117153345_create_subscriptions.rb
@@ -1,7 +1,7 @@
 class CreateSubscriptions < ActiveRecord::Migration[5.0]
   def change
     create_table :subscriptions do |t|
-      t.string :type
+      t.string :subscription_type
       t.timestamps
     end
   end
