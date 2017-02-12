978c1baac1804703a49814408551c07261da0cc1
damzcodes <damzcodes@gmail.com>
2017-02-12 12:04:54 +0000

    add devise views, allow users to see & edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   # devise_scope :user do
   #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   # end
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   get '/subscribe' => "static_pages#subscribe"
 
   resources :events
+  resources :users
 end
