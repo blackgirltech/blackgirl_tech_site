ac53ca0b226bcf28cd9fdbfab82ab4354211bfbd
damzcodes <damzcodes@gmail.com>
2017-02-12 13:04:35 +0000

    user can log in & log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-  # devise_scope :user do
-  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  # end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#about'
   get '/about' => "static_pages#about"
@@ -11,5 +9,5 @@ Rails.application.routes.draw do
   get '/subscribe' => "static_pages#subscribe"
 
   resources :events
-  resources :users
+  resources :users, except: :destroy
 end
