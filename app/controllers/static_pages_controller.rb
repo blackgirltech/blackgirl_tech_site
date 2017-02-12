3e8e6affa438295102da165ec48b76dd3d020322
damzcodes <damzcodes@gmail.com>
2017-02-12 23:17:53 +0000

    move newsletter subscribe to footer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,15 +4,8 @@ class StaticPagesController < ApplicationController
   end
   
   def about
-    @event = Event.last
   end
 
   def code_of_conduct
-  end
-
-  def donate
-  end
-
-  def subscribe
   end
 end
