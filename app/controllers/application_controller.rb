82c2873b91b87978bf66fb2aaf194814549d75fc
damzcodes <damzcodes@gmail.com>
2017-02-12 14:42:15 +0000

    Admin can create events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def admin?
+    if current_user && current_user.admin?
+      true
+    else
+      false
+    end
+  end
+
+  def volunteer?
+    if current_user && current_user.volunteer?
+      true
+    else
+      false
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def admin?
+    if current_user && current_user.admin?
+      true
+    else
+      false
+    end
+  end
+
+  def volunteer?
+    if current_user && current_user.volunteer?
+      true
+    else
+      false
+    end
+  end
 end
