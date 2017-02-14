977b92f0c2a35b266a82ea56c1b0946744ca7df6
damzcodes <damzcodes@gmail.com>
2017-02-14 16:26:19 +0000

    display subscription info to active admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,24 +1,30 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation, :volunteer, :admin, :name, :provider, :uid, :subscription_id
+  permit_params :email, :password, :password_confirmation, :volunteer, :admin, :name, :provider, :uid,:subscription_id
 
   index do
     selectable_column
     id_column
     column :email
     column :volunteer
+    column :subscription do |user|
+      if user.subscription.present?
+        user.subscription.subscription_type
+      end
+    end
+
     column :created_at
     actions
   end
 
   filter :email
   filter :volunteer
+  filter :subscription
   filter :created_at
 
   form do |f|
-    f.inputs "Admin Details" do
+    f.inputs do
       f.input :email
-      f.input :password
-      f.input :password_confirmation
+      f.input :subscription, as: :select, collection: Subscription.all.map {|su| [su.subscription_type, su.id]}
       f.input :volunteer
       f.input :admin
     end
