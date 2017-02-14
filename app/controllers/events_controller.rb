e32b0d6018b03b671a25a11c9b570c354afc5283
damzcodes <damzcodes@gmail.com>
2017-02-14 16:41:17 +0000

    move active admin assets to vendor

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,26 +1,8 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, :except => [:show, :index]
-
-  def create
-    Event.create!(event_params)
-    redirect_to events_path
-  rescue ActiveRecord::RecordInvalid => e
-    @event = e.record
-    render :index
-  end
-
-  def new
-    @event = Event.new
-  end
 
   def index
     @workshops = Event.where(workshop: true).order(date: :asc)
     @masterclasses = Event.where(masterclass: true).order(date: :asc)
   end
   
-  private
-
-  def event_params
-    params.require(:event).permit(:name, :details, :image, :address, :workshop, :masterclass, :date, :time, :url)
-  end
 end
