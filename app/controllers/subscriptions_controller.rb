8d33a463dcbc158142da8f034f6e4518700115f3
damzcodes <damzcodes@gmail.com>
2017-02-27 22:43:16 +0000

    user can subscribe)

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -8,6 +8,8 @@ class SubscriptionsController < ApplicationController
     # expiration date should be Date.today + 1.year
     # how will we create with a mutliple options? Might have to manually post to create in href on new page
     @subscription = Subscription.create!(subscription_params)
+    a_year_from_today = Date.today + 1.year
+    @subscription.update_attribute(:expiration_date, a_year_from_today)
     redirect_to root_path
   end
 
