ee428edfeb7139970d0ee1202866fe6e0d37ca4c
damzcodes <damzcodes@gmail.com>
2017-02-28 00:18:57 +0000

    signed in user can subscribe

--- db/migrate/20170228001533_add_user_id_to_subscriptions.rb
+++ db/migrate/20170228001533_add_user_id_to_subscriptions.rb
@@ -1,5 +1,5 @@
 class AddUserIdToSubscriptions < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :user_id, :integer
+    add_column :subscriptions, :user_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221194318) do
+ActiveRecord::Schema.define(version: 20170228001533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20170221194318) do
     t.string   "redirect_url"
     t.string   "response_id"
     t.datetime "expiration_date"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -79,7 +80,6 @@ ActiveRecord::Schema.define(version: 20170221194318) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
-    t.integer  "user_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
