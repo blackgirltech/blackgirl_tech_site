def3e71d65f2c0de03d7d50f73089d9beb587794
damzcodes <damzcodes@gmail.com>
2017-03-05 15:32:55 +0000

    add cancellation column to subscriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228120343) do
+ActiveRecord::Schema.define(version: 20170305153155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20170228120343) do
     t.string   "response_id"
     t.datetime "expiration_date"
     t.integer  "user_id"
+    t.datetime "cancellation_date"
   end
 
   create_table "users", force: :cascade do |t|
