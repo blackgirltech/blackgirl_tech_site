6e7ee1519efd895ccbc42cfbdb78040d14c3ebc8
damzcodes <damzcodes@gmail.com>
2017-05-05 10:30:00 +0100

    refractor create method

--- lib/create_subscription.rb
+++ lib/create_subscription.rb
@@ -7,19 +7,22 @@ class CreateSubscription
 
   def create(user, params)
     if params[:subscription_type] == "FREE"
-      # subscription = user.subscriptions.create!(subscription_type: params[:subscription_type], expiration_date: Date.today + 1.year)
       FreeSubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
       return
     elsif params[:subscription_type] == "MEMBER"
       subscription = MemberSubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
-      response = client.redirect_flow(subscription.subscription_type, params[:authenticity_token], params[:success_redirect_url])
-      subscription.update!(redirect_url: response.redirect_url, response_id: response.id)
-      subscription
+      get_response_from_payment_gateway
     elsif params[:subscription_type] == "ALLY"
       subscription = AllySubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
-      response = client.redirect_flow(subscription.subscription_type, params[:authenticity_token], params[:success_redirect_url])
-      subscription.update!(redirect_url: response.redirect_url, response_id: response.id)
-      subscription
+      get_response_from_payment_gateway
     end
+  end
+
+  private
+
+  def get_response_from_payment_gateway
+    response = client.redirect_flow(subscription.subscription_type, params[:authenticity_token], params[:success_redirect_url])
+    subscription.update!(redirect_url: response.redirect_url, response_id: response.id)
+    subscription
   end
 end
