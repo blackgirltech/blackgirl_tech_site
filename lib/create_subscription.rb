d7e38efc33d497beb1e0f704f0c7fc5e9e4f6712
damzcodes <damzcodes@gmail.com>
2017-05-05 17:53:12 +0100

    pass vars as args

--- lib/create_subscription.rb
+++ lib/create_subscription.rb
@@ -10,19 +10,19 @@ class CreateSubscription
       FreeSubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
       return
     elsif params[:subscription_type] == "MEMBER"
-      @subscription = MemberSubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
-      get_response_from_payment_gateway
+      subscription = MemberSubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
+      get_response_from_payment_gateway(subscription, params)
     elsif params[:subscription_type] == "ALLY"
-      @subscription = AllySubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
-      get_response_from_payment_gateway
+      subscription = AllySubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
+      get_response_from_payment_gateway(subscription, params)
     end
   end
 
   private
 
-  def get_response_from_payment_gateway
+  def get_response_from_payment_gateway(subscription, params)
     response = client.redirect_flow(subscription.subscription_type, params[:authenticity_token], params[:success_redirect_url])
-    @subscription.update!(redirect_url: response.redirect_url, response_id: response.id)
-    @subscription
+    subscription.update!(redirect_url: response.redirect_url, response_id: response.id)
+    subscription
   end
 end
