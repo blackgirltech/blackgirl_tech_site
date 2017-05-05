ec1a8cb9026df8d5341ddde06a26a531ac271326
damzcodes <damzcodes@gmail.com>
2017-05-05 17:50:12 +0100

    change vars to instance vars

--- lib/create_subscription.rb
+++ lib/create_subscription.rb
@@ -10,10 +10,10 @@ class CreateSubscription
       FreeSubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
       return
     elsif params[:subscription_type] == "MEMBER"
-      subscription = MemberSubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
+      @subscription = MemberSubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
       get_response_from_payment_gateway
     elsif params[:subscription_type] == "ALLY"
-      subscription = AllySubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
+      @subscription = AllySubscription.create!(subscription_type: params[:subscription_type], user_id: user.id, expiration_date: Date.today + 1.year)
       get_response_from_payment_gateway
     end
   end
@@ -22,7 +22,7 @@ class CreateSubscription
 
   def get_response_from_payment_gateway
     response = client.redirect_flow(subscription.subscription_type, params[:authenticity_token], params[:success_redirect_url])
-    subscription.update!(redirect_url: response.redirect_url, response_id: response.id)
-    subscription
+    @subscription.update!(redirect_url: response.redirect_url, response_id: response.id)
+    @subscription
   end
 end
