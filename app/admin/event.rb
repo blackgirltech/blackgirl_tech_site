13af887d6831e504ab6ac22eb708b5dddf725b18
damzcodes <damzcodes@gmail.com>
2017-06-16 11:28:06 +0100

    venue displaying as select in event form

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Event do
 
-  permit_params :name, :date, :time, :address, :details, :image, :url, :workshop, :masterclass
+  permit_params :name, :date, :time, :address, :details, :image, :url, :workshop, :masterclass, :venue_id
 
   filter :name
   filter :created_at
@@ -20,6 +20,21 @@ ActiveAdmin.register Event do
     actions
   end
 
+  show do
+    attributes_table do
+      row :name
+      row :date
+      row :time
+      row :venue do |event|
+        if event.venue.present?
+          event.venue.first.name
+        end
+      end
+      row :workshop
+      row :masterclass
+    end
+  end
+
   form do |f|
     f.inputs do
       f.input :name
@@ -27,7 +42,7 @@ ActiveAdmin.register Event do
       f.input :time
       f.input :details
       f.input :image
-      f.input :venue, as: :select, collection: Venue.pluck(:name)
+      f.select :venue, Venue.pluck(:name, :id)
       f.input :workshop
       f.input :masterclass
     end
