526584895173013ed030d95af31950ab7247375d
damzcodes <damzcodes@gmail.com>
2017-08-06 15:14:02 +0100

    Move stripe global vars into function

--- app/assets/javascripts/stripe.js
+++ app/assets/javascripts/stripe.js
@@ -1,58 +1,59 @@
-var stripe = Stripe(stripe_env);
-var elements = stripe.elements();
+function initialiseStripe(stripe_env, form_id) {
+  var stripe = Stripe(stripe_env);
+  var elements = stripe.elements();
 
+  // Custom styling can be passed to options when creating an Element.
+  var style = {
+    base: {
+      // Add your base input styles here. For example:
+      fontSize: '16px',
+      lineHeight: '24px'
+    }
+  };
 
-// Custom styling can be passed to options when creating an Element.
-var style = {
-  base: {
-    // Add your base input styles here. For example:
-    fontSize: '16px',
-    lineHeight: '24px'
-  }
-};
+  // Create an instance of the card Element
+  var card = elements.create('card', {hidePostalCode: true}, {style: style});
 
-// Create an instance of the card Element
-var card = elements.create('card', {hidePostalCode: true}, {style: style});
+  // Add an instance of the card Element into the `card-element` <div>
+  card.mount('#card-element');
 
-// Add an instance of the card Element into the `card-element` <div>
-card.mount('#card-element');
-
-card.addEventListener('change', function(event) {
-  var displayError = document.getElementById('card-errors');
-  if (event.error) {
-    displayError.textContent = event.error.message;
-  } else {
-    displayError.textContent = '';
-  }
-});
-
-// Create a token or display an error when the form is submitted.
-var form = document.getElementById(form_id);
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  stripe.createSource(card).then(function(result) {
-    if (result.error) {
-      // Inform the user if there was an error
-      var errorElement = document.getElementById('card-errors');
-      errorElement.textContent = result.error.message;
+  card.addEventListener('change', function(event) {
+    var displayError = document.getElementById('card-errors');
+    if (event.error) {
+      displayError.textContent = event.error.message;
     } else {
-      // Send the source to your server
-      stripeSourceHandler(result.source);
+      displayError.textContent = '';
     }
   });
-});
 
-function stripeSourceHandler(source) {
-  // Insert the token ID into the form so it gets submitted to the server
+  // Create a token or display an error when the form is submitted.
   var form = document.getElementById(form_id);
-  var hiddenInput = document.createElement('input');
-  hiddenInput.setAttribute('type', 'hidden');
-  hiddenInput.setAttribute('name', 'stripe_source');
-  hiddenInput.setAttribute('value', source.id);
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-  form.appendChild(hiddenInput);
+    stripe.createSource(card).then(function(result) {
+      if (result.error) {
+        // Inform the user if there was an error
+        var errorElement = document.getElementById('card-errors');
+        errorElement.textContent = result.error.message;
+      } else {
+        // Send the source to your server
+        stripeSourceHandler(result.source);
+      }
+    });
+  });
 
-  // Submit the form
-  form.submit();
+  function stripeSourceHandler(source) {
+    // Insert the token ID into the form so it gets submitted to the server
+    var form = document.getElementById(form_id);
+    var hiddenInput = document.createElement('input');
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', 'stripe_source');
+    hiddenInput.setAttribute('value', source.id);
+
+    form.appendChild(hiddenInput);
+
+    // Submit the form
+    form.submit();
+  }
 }
