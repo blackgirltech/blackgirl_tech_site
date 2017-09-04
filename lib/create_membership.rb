c4745b1a78751071e164dde62b97d9e3559c8bc7
damzcodes <damzcodes@gmail.com>
2017-09-04 18:30:46 +0100

    nil check for base_membership

--- lib/create_membership.rb
+++ lib/create_membership.rb
@@ -9,9 +9,9 @@ class CreateMembership
     @client = StripePayment.new
 
     # Cancel base membership before creating paid membership
-    if member.memberships.find_by_membership_type("BASE").cancellation_date.nil?
-      old_membership = member.memberships.find_by_membership_type("BASE")
-      old_membership.update!(cancellation_date: Time.now)
+    base_membership = member.memberships.find_by_membership_type("BASE")
+    if !base_membership.nil? && base_membership.cancellation_date.nil?
+      base_membership.update!(cancellation_date: Time.now)
     end
 
     if member.stripe_customer_id.nil? || member.stripe_source.nil?
