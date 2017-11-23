2fe32803f1606b2f10a9c30a2647272d5c846b41
damzcodes <damzcodes@gmail.com>
2017-11-23 17:17:02 +0000

    CRUD resources

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,20 +1,41 @@
 class ResourcesController < ApplicationController
 
   def index
+    @resources = Resource.all
   end
 
   def new
+    # authenticate_admin!
+    @resource = Resource.new
   end
 
   def create
+    # authenticate_admin!
+    @resource = Resource.create!(resource_params)
+    redirect_to resources_path
   end
 
   def edit
+    # authenticate_admin!
+    @resource = Resource.find(params[:id])
   end
 
   def update
+    # authenticate_admin!
+    @resource = Resource.find(params[:id])
+    @resource.update(resource_params)
+    redirect_to resources_path
   end
 
-  def delete
+  def destroy
+    # authenticate_admin!
+    @resource = Resource.find(params[:id])
+    @resource.delete
+    redirect_to resources_path
+  end
+
+  private
+  def resource_params
+    params.require(:resource).permit(:title, :preview_description, :description, :external_url)
   end
 end
---
+++ app/models/resource.rb
@@ -0,0 +1,3 @@
+class Resource < ApplicationRecord
+
+end
