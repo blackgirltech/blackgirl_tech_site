df203a0e05144ab92f086f8b55141e88ffb044be
damzcodes <damzcodes@gmail.com>
2017-11-27 11:50:44 +0000

    remove forest

--- app/controllers/forest/rsvps_controller.rb
+++
@@ -1,8 +0,0 @@
-class Forest::RsvpsController < ForestLiana::ApplicationController
-  def check_in
-    @rsvps = []
-    @rsvps << Rsvp.where(id: params[:data][:attributes][:ids])
-    @rsvps.each { |r| r.update(checked_in: true) }
-    render nothing: true, status: 204
-  end
-end
