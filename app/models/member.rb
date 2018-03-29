d09e20fc6dbd9ea7e8844780706d2e1ccf556233
damzcodes <damzcodes@gmail.com>
2018-03-29 16:52:41 +0100

    double opt-in

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable,
          omniauth_providers: [:facebook, :twitter, :google_oauth2]
 
   has_many :rsvps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329151910) do
+ActiveRecord::Schema.define(version: 20180329154219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,10 @@ ActiveRecord::Schema.define(version: 20180329151910) do
     t.string   "stripe_customer_id"
     t.boolean  "black_woman_nbp",        default: false
     t.boolean  "contact_permission"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.index ["email"], name: "index_members_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true, using: :btree
   end
