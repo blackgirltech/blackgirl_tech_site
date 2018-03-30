e65d8a938f706ce83d028b240d2e7213973d9938
damzcodes <damzcodes@gmail.com>
2018-03-30 20:52:28 +0100

    autoload path

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module BlackgirlTechSite
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.enable_dependency_loading = true
     config.autoload_paths << Rails.root.join("lib")
     config.active_job.queue_adapter = :delayed_job
     config.serve_static_assets = true
