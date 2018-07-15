af72f47fe7e5407595befed7f3b9a82f0138a892
damzcodes <damzcodes@gmail.com>
2018-07-15 20:23:56 +0100

    refactor stripe methods

--- lib/event_payment.rb
+++ lib/event_payment.rb
@@ -7,19 +7,14 @@ class EventPayment
 
   def pay(member, event, rsvp, stripe_source)
     if member.nil?
-      charge = Stripe::Charge.create(
-        :amount => event.price_in_pence,
-        :currency => "gbp",
-        :description => "unregistered_member_rsvp",
-        :source => stripe_source,
-      )
+      charge = @client.create_charge(event.price_in_pence, stripe_source)
     elsif member.stripe_customer_id.nil? || member.stripe_source.nil?
       member.update(stripe_source: stripe_source)
       customer = @client.create_customer(member)
       member.update(stripe_customer_id: customer.id)
-      charge = @client.create_charge(customer.id, event, rsvp.donate)
+      charge = @client.create_customer_charge(customer.id, event, rsvp.donate)
     else
-      charge = @client.create_charge(member.stripe_customer_id, event, rsvp.donate)
+      charge = @client.create_customer_charge(member.stripe_customer_id, event, rsvp.donate)
     end
 
     rsvp.update(stripe_charge_token: charge.id)
