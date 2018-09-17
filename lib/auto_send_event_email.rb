7eba0f305d7d2d40cd2de1ee068f74a2a8b273ab
lolaodelola <damzcodes@gmail.com>
2018-09-17 18:25:25 +0100

    it's a class method, duh

--- lib/auto_send_event_email.rb
+++ lib/auto_send_event_email.rb
@@ -1,5 +1,5 @@
 class AutoSendEventEmail
-  def send_email(event)
+  def self.send_email(event)
     Member.where(subscribed_to_email: true).map do |member|
       EventMailer.delay.upcoming_event_email(member, event)
     end
