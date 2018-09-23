3d09c3ec63ca9c00923f95e3086f70a7fdfd2991
lolaodelola <damzcodes@gmail.com>
2018-09-23 17:18:34 +0100

    new & create actions

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationsController < ApplicationController
 
   def new
+    @opportunity = Opportunity.find(params[:opportunity_id])
+    @application = Application.new
   end
 
   def create
+    @application = Application.create(application_params)
+    redirect_to opportunities_path
   end
 
   def edit
@@ -12,11 +16,12 @@ class ApplicationsController < ApplicationController
   def update
   end
 
-  def index
-    @applications = Application.all
-  end
-
   def show
     @application = Application.find(params[:id])
+  end
+
+  private
+  def application_params
+    params.require(:application).permit(:opportunity_id, :member_id, :cv, :longlist, :shortlist, :finalist, :awarded)
   end
 end
